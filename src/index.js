fc3116940e68d32e3cc95efa6d6bec4394b738d5
HanLander <157143716+HanLander@users.noreply.github.com>
2024-06-11 18:48:11 +0100

    Added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "The recipe will go here",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+  recipeElement.innerHTML = "";
+}
+
+let recipeFormElemment = document.querySelector("#recipe-generator-form");
+recipeFormElemment.addEventListener("submit", generateRecipe);
