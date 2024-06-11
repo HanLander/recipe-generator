3be46c87ad2288b5c56a2adb25a8f92a8e74d12d
HanLander <157143716+HanLander@users.noreply.github.com>
2024-06-11 19:46:57 +0100

    Added AI API integration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
-function generateRecipe(event) {
-  event.preventDefault();
+function displayRecipe(response) {
+  console.log("recipe generated");
 
   new Typewriter("#recipe", {
-    strings: "The recipe will go here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
-  recipeElement.innerHTML = "";
+}
+
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "d4c90a6e0890oeaa244ft334db3e3663";
+  let context =
+    "You are a culinary expert and have lots of knowledge about food. Your mission is to write recipes in basic HTML. Make sure to use the user instructions.";
+  let prompt = `User instructions: Generate a recipe based on the ingredient entered ${instructionsInput.value} using the metric measurements`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("generating recipe");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+  axios.get(apiURL).then(displayRecipe);
 }
 
 let recipeFormElemment = document.querySelector("#recipe-generator-form");
